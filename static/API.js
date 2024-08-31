5c081fb592858e05e7104429d791505a08176fc9
serrer6 <serrer6@outlook.com>
2024-08-31 22:53:35 +0800

    new file:   index.html 	new file:   static/API.js 	new file:   static/color.css 	new file:   static/fonts.css 	new file:   static/fonts/AlimamaFangYuanTiVF.ttf 	new file:   static/footer/footer.css 	new file:   static/iconfont/iconfont.css 	new file:   static/iconfont/iconfont.ttf 	new file:   static/iconfont/iconfont.woff 	new file:   static/iconfont/iconfont.woff2 	new file:   static/img/logo.png 	new file:   static/main.css 	new file:   static/main.js 	new file:   static/page/dynamic.css 	new file:   static/page/dynamic.js 	new file:   static/page/index.css 	new file:   test.html 	new file:   viewer/dynamic.html

---
+++ static/API.js
@@ -0,0 +1,5 @@
+var APIUrl = "http://127.0.0.1:8081"
+
+function ApiRequester(ReqUrl,Mode){
+    
+}
---
+++ static/main.js
@@ -0,0 +1,12 @@
+var Message_Service_Url = APIUrl + "/v1/feature/get/message";
+
+
+var Message_Service_Req = new XMLHttpRequest();
+Message_Service_Req.open('GET',Message_Service_Url);
+Message_Service_Req.send();
+Message_Service_Req.onload = function () {
+    let Message_Service_Ele= document.getElementById("message-box");
+    const Message_Service_Obj = JSON.parse(Message_Service_Req.response);
+    Message_Service_Ele.innerHTML = Message_Service_Obj.data.message;
+}
+
